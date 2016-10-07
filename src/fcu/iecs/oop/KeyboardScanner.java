8d0e9f6632576621dc4d41d9f3b64a5b8719f449
user <user@FF10>
2016-10-07 09:18:22 +0800

    KeyboardScanner 修改

--- src/fcu/iecs/oop/KeyboardScanner.java
+++ src/fcu/iecs/oop/KeyboardScanner.java
@@ -1,10 +1,23 @@
 package fcu.iecs.oop;
-
-public class KeyboardScanner {
+import java.util.Scanner;
+public class PrintNumberInWord {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		int a ;
+		float b ;
+		String c ;
 
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Enter a integer: ");
+		a =scanner.nextInt();
+		System.out.println("Enter a float point number: ");	
+		b =scanner.nextFloat();
+		System.out.println("Enter a you name: ");
+		c =scanner.next();
+		System.out.printf("Hi %s, the multiplication of %d and  %8.5f is %8.2e . ",c,a,b,a*b);
+		
+	}
 }
